35b9bba8c3c7e682630154d2a52078ef5e5eae06
Zen <caiyuzhen00@gmail.com>
2023-02-13 01:22:31 +0800

    build:view

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { cursorInit } from './components/Cursor/cursor.js'
+import baseInfoView from './views/baseInfoView.js'
 
 
-// 初始化, 在拉伸的时候不会影响到画布内的元素比例
-cursorInit()
+
+// 渲染根页面
+((doc) => {
+	const oAppContainer = doc.querySelector('#app')
+
+
+	const init = () => {
+		// 渲染页面
+		oAppContainer.innerHTML = baseInfoView()
+		
+		// 初始化, 在拉伸的时候不会影响到画布内的元素比例
+		cursorInit()
+	}
+
+	init()
+})(document)
+
+
---
+++ src/views/baseInfoView.js
@@ -0,0 +1,32 @@
+
+const baseInfoTemp = `
+	<div class="container">
+
+		<div class="info-header">
+			<div class="info-header-nav">
+				<span class="info-header-nav-title">Portfolio</span>
+				<div class="info-header-nav-badge">
+					<div>badge</div>
+					<div>switch</div>
+				</div>
+			</div>
+
+			<div class="info-header-text">
+				<p>Hi, I'm a frontend development and designer.</p>
+				<span>more about me</span>
+			</div>
+			<div class="info-header-imgs">
+				<div class="info-header-imgs-img">Image</div>
+				<span class="info-header-imgs-rotate">Rotate</span>
+			</div>
+		</div>
+
+		<div class="part2"></div>
+		<div class="part3"></div>
+	</div>
+`
+
+
+export default function baseInfoView() {
+	return baseInfoTemp
+}
