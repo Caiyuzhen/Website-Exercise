7bb06a4989f2c41c8dc9a662c77609ad85f4fe52
yuzhen <caiyuzhen00@gmail.com>
2023-02-14 14:02:27 +0800

    build:view

--- src/views/baseInfoView.js
+++ src/views/baseInfoView.js
@@ -1,7 +1,7 @@
 // 引入图片
 let circleBadgeNormal = './imgs/cssda-special-kudos-black.png'
 let circleBadgeHover = './imgs/cssda-special-kudos-purple.png'
-
+import bigTitle from "../views/bigTitle.js" //🔥路径需要从最外侧调用！
 
 // 顶部信息 View
 const baseInfoTemp = `
@@ -10,14 +10,19 @@ const baseInfoTemp = `
 			<div class="info-header-nav">
 				<span class="info-header-nav-title">Portfolio</span>
 				<div class="info-header-nav-badge">
+
 					<!-- 🔥🔥🔥打标记的思路: 只要有 data-bigdot="true" 这个属性的元素 hover 上去后都会改变 Point 的样式！-->
+					<!-- 只要加上 data-bigdot="true" 就有 hover 效果!-->
 					<div data-bigdot="true" class="info-header-nav-badge-box">
 						<img class="circleBadgeNormal" src="${circleBadgeNormal}" alt="">
 						<img class="circleBadgeHover" src="${circleBadgeHover}" alt="">
 					</div>
+					
+					<!-- 按钮 -->
 					<div class="info-header-switch-container">
 						<!-- 右侧开关 -->
-						<div class="info-header-nav-badge-switch">
+						<!-- 只要加上 data-bigdot="true" 就有 hover 效果!-->
+						<div data-bigdot="true" class="info-header-nav-badge-switch">
 							<!-- 🔥🔥🔥开关小技巧, 把 checkbox type 的 input 放到 label 内！ 【点击 switch-bar】 也能改变 【checkbox】的状态！！因为在 label 下整个会自动绑定！ -->
 							<label>
 								<input class="checkBoxSwitch" type="checkbox">
@@ -33,10 +38,10 @@ const baseInfoTemp = `
 				</div>
 			</div>
 
-			<div class="info-header-text">
-				<p>Hi, I'm a frontend development and designer.</p>
-				<span>more about me</span>
-			</div>
+			<!-- 大标题文字组件 -->
+			${bigTitle()}
+			
+			
 			<div class="info-header-imgs">
 				<div class="info-header-imgs-img">Image</div>
 				<span class="info-header-imgs-rotate">Rotate</span>
---
+++ src/views/bigTitle.js
@@ -0,0 +1,14 @@
+const bigTitleInfo = `
+	<div class="info-header-detail" data-bigdot="true">
+		<span>HI ——</span>
+		<span>I'M ZENO, FRONTEDN</span>
+		<span>DEVELOPER AND DESIGNER</span>
+		<span class="info-header-more">
+			more about me
+		</span>
+	</div>
+`
+
+export default function bigTitle () {
+	return bigTitleInfo
+}
